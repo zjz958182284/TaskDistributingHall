c61b3abb261a0f5bb3b1e9cf7b5221948c2f971a
gi3636 <fenggi123@gmail.com>
2020-06-03 11:23:14 +0800

    首页大厅界面流程完成

--- app/src/main/java/com/example/taskdistributinghall/Fragment/Home/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/taskdistributinghall/Fragment/Home/RecyclerViewAdapter.java
@@ -1,15 +1,17 @@
 package com.example.taskdistributinghall.Fragment.Home;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.taskdistributinghall.Mission_detail_page;
 import com.example.taskdistributinghall.R;
 
 import java.util.List;
@@ -39,9 +41,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(),
-                    titleList.get(getAdapterPosition()), Toast.LENGTH_LONG)
-                    .show();
+          Context context= view.getContext();
+          Intent intent =new Intent(context, Mission_detail_page.class);
+          context.startActivity(intent);
         }
     }
 
